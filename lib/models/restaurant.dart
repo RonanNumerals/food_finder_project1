b255ba72951bb992b366f6532349e07bf0eb4f85
Ronan Pelot <ronanpelot@gmail.com>
2026-03-21 23:32:53 -0400

    Added more restaurant data, updated database helper with Read functions for menu items

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -5,9 +5,10 @@ class Restaurant {
   final String imagePath;
   final List<MenuItem> menuItems;
   final String description;
-  final List<String> hours;
+  final String hours;
   final String location;
   final double rating;
+  final int priceLevel; // 1 = $, 2 = $$, 3 = $$$
 
   Restaurant({
     required this.name,
@@ -17,5 +18,8 @@ class Restaurant {
     required this.hours,
     required this.location,
     required this.rating,
+    required this.priceLevel,
   });
+
+  String get priceLabelString => '\$' * priceLevel;
 }
